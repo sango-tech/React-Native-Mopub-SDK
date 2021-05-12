4a09acb1d90981e18d301564a840ecff3328f2ca
abinhho <abinh.ho@gmail.com>
2021-05-12 16:01:27 +0700

    Update RNNativeAdView.h

--- ios/RNNativeAdView.h
+++ ios/RNNativeAdView.h
@@ -8,7 +8,7 @@
 #import "RCTView.h"
 #import "MPNativeAdRendering.h"
 #import "MPNativeAdConstants.h"
-#import <RCTTextView.h>
+#import "RCTTextView.h"
 #import <mopub-ios-sdk/MoPub.h>
 NS_ASSUME_NONNULL_BEGIN
 
